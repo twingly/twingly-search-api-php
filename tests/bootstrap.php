6dd8ff5fc13428c85ce0edbd7b6d1c5a4a7ff2a2
Bearburger <unrarme@yandex.ru>
2016-02-08 14:22:14 +0300

    Fix #3 - added default date timezone declaration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-// Set timezone
-date_default_timezone_set('Europe/London');
-
-
 // Enable Composer autoloader
 /** @var \Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
