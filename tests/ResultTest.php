c789b432076536ceff8aa094f65ac936ad8e17ab
Robin Wallin <robin@twingly.com>
2016-05-02 10:07:20 +0200

    Fix mistake in fixture
    The fixture holds three posts. Extend existing test to check that we
    find the correct numbers from the “header element”.

--- tests/ResultTest.php
+++ tests/ResultTest.php
@@ -18,8 +18,11 @@ class ResultTest extends \PHPUnit_Framework_TestCase {
         $r = (new Parser())->parse($data);
         $this->assertTrue(is_array($r->posts));
         $this->assertTrue(is_int($r->number_of_matches_total));
+        $this->assertEquals($r->number_of_matches_total, 60768);
         $this->assertTrue(is_int($r->number_of_matches_returned));
+        $this->assertEquals($r->number_of_matches_returned, 1000);
         $this->assertTrue(is_float($r->seconds_elapsed));
+        $this->assertEquals($r->seconds_elapsed, 0.022);
         $this->assertFalse($r->all_results_returned());
     }
 }
