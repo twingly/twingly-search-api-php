20e83c3bf40a735f2e6d472ddb02aa0a4533a2e8
Jan Beckmann <king-jan1999@hotmail.de>
2017-05-12 12:22:43 +0200

    add/change tests according to ruby version

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -12,99 +12,30 @@ use Twingly\Exception;
 use Twingly\Query;
 use Twingly\QueryException;
 
-class ParserTest extends \PHPUnit_Framework_TestCase
-{
-    static function getFixture($fixture_name)
-    {
+class ParserTest extends \PHPUnit_Framework_TestCase {
+    static function getFixture($fixture_name) {
         $file_path = dirname(__DIR__) . "/tests/fixtures/{$fixture_name}.xml";
         return file_get_contents($file_path);
     }
 
-    function testWithValidResult()
-    {
+    function testWithValidResult() {
         $data = self::getFixture('minimal_valid_result');
         $r = (new Parser())->parse($data);
         $this->assertTrue($r instanceof Result);
-    }
+        $this->assertEquals(3, $r->number_of_matches_returned);
+        $this->assertEquals(3122050, $r->number_of_matches_total);
+        $this->assertEquals(0.369, $r->seconds_elapsed);
 
-    function testWithValidEmptyResult()
-    {
-        $data = self::getFixture('valid_empty_result');
-        $r = (new Parser())->parse($data);
-        $this->assertTrue($r instanceof Result);
-        $this->assertEquals(count($r->posts), 0);
-        $this->assertEquals($r->number_of_matches_total, 0);
-        $this->assertEquals($r->number_of_matches_returned, 0);
-    }
-
-    function testWithNonexistentApiKeyResult()
-    {
-        try {
-            $data = self::getFixture('nonexistent_api_key_result');
-            $r = (new Parser())->parse($data);
-            $this->fail('Should throw Auth exception');
-        } catch (QueryException $e) {
-            $this->assertEquals('Parameter apikey may not be empty (code: 40001)', $e->getMessage());
-        }
-    }
-
-    function testWithUnauthorizedApiKeyResult()
-    {
-        try {
-            $data = self::getFixture('unauthorized_api_key_result');
-            $r = (new Parser())->parse($data);
-            $this->fail('Should throw Auth exception');
-        } catch (AuthException $e) {
-            $this->assertEquals('Unauthorized (code: 40101)', $e->getMessage());
-        }
-    }
-
-    function testWithServiceUnavailableResult()
-    {
-        try {
-            $data = self::getFixture('service_unavailable_result');
-            $r = (new Parser())->parse($data);
-            $this->fail('Should throw Server exception');
-        } catch (ServerException $e) {
-            $this->assertEquals('Authentication service unavailable (code: 50301)', $e->getMessage());
-        }
-    }
-
-    function testWithUndefinedErrorResult()
-    {
-        try {
-            $data = self::getFixture('undefined_error_result');
-            $r = (new Parser())->parse($data);
-            $this->fail('Should throw Server exception');
-        } catch (ServerException $e) {
-            $this->assertEquals('Internal Server Error (code: 50001)', $e->getMessage());
-        }
-    }
-
-    function testWithNonXmlResult()
-    {
-        try {
-            $data = self::getFixture('non_xml_result');
-            $r = (new Parser())->parse($data);
-            $this->fail('Should throw Server exception');
-        } catch (ServerException $e) {
-            $this->assertEquals('503 Service Unavailable', $e->getMessage());
-        }
-    }
-
-    function testWithPostContainingOneTag()
-    {
-        $data = self::getFixture('minimal_valid_result');
-        $result = (new Parser())->parse($data);
-        $actual_post = $result->posts[0];
-
+        $actual_post = $r->posts[0];
         $expected_post = new Post();
+        $this->assertStringStartsWith("Det vart en förmiddag på boxen med en brud som jag", $actual_post->text);
+        $actual_post->text = "";
         $expected_post->set_values([
             "id" => '16405819479794412880',
             "author" => "klivinihemligheten",
             "url" => "http://nouw.com/klivinihemligheten/planering---men-dalig-30016048",
             "title" => "Planering - men dålig",
-            "text" => "Det vart en förmiddag på boxen med en brud som jag lärt känna där. Körde en egen WOD, bland annat SDHP, shoulder press, HSPU - bland annat. Hade planerat dagen in i minsta detalj, insåg under passet att jag glömt leggings. Så - det var bara att sluta lite tidigare för att röra sig hemåt för dusch och lunch. Har alltså släpat med mig ryggsäcken med allt för dagen i onödan. Riktigt riktigt klantigt! Har nu en timma på mig att duscha och göra mig ordning för föreläsning, innan det är dags att dra igen. Och jag som skulle plugga innan", // sorry!
+            "text" => "",
             "languageCode" => "sv",
             "locationCode" => "se",
             "coordinates" => [],
@@ -121,12 +52,100 @@ class ParserTest extends \PHPUnit_Framework_TestCase
             "blogRank" => "1",
             "tags" => ["Ätas & drickas", "Universitet & studentlivet", "Träning", "To to list"],
         ]);
-
         $this->assertEquals($expected_post, $actual_post);
+
+        // repeat with last post
+        $actual_post = $r->posts[count($r->posts) - 1];
+
+        $this->assertStringStartsWith("Hmm.... Vad ska man börja ??  Jag vet inte riktigt vad min gnista", $actual_post->text);
+        $actual_post->text = "";
+
+        $expected_post->set_values([
+            "id" => '2770252465384762934',
+            "author" => "maartiinasvardag",
+            "url" => "http://nouw.com/maartiinasvardag/god-formiddag-30016041",
+            "title" => "God förmiddag! ☀",
+            "text" => "",
+            "languageCode" => "sv",
+            "locationCode" => "se",
+            "coordinates" => [],
+            "images" => [],
+            "links" => [],
+            "publishedAt" => "2017-05-04T06:49:50Z",
+            "indexedAt" => "2017-05-04T06:50:07Z",
+            "reindexedAt" => "0001-01-01T00:00:00Z",
+            "inlinksCount" => 0,
+            "blogId" => '1578135310841173675',
+            "blogName" => "maartiinasvardag blogg",
+            "blogUrl" => "http://nouw.com/maartiinasvardag",
+            "authority" => "0",
+            "blogRank" => "1",
+            "tags" => [],
+        ]);
+
     }
 
-    function testWithLinks()
-    {
+
+    function testWithValidEmptyResult() {
+        $data = self::getFixture('valid_empty_result');
+        $r = (new Parser())->parse($data);
+        $this->assertTrue($r instanceof Result);
+        $this->assertEquals(count($r->posts), 0);
+        $this->assertEquals($r->number_of_matches_total, 0);
+        $this->assertEquals($r->number_of_matches_returned, 0);
+    }
+
+    function testWithNonexistentApiKeyResult() {
+        try {
+            $data = self::getFixture('nonexistent_api_key_result');
+            $r = (new Parser())->parse($data);
+            $this->fail('Should throw Auth exception');
+        } catch (QueryException $e) {
+            $this->assertEquals('Parameter apikey may not be empty (code: 40001)', $e->getMessage());
+        }
+    }
+
+    function testWithUnauthorizedApiKeyResult() {
+        try {
+            $data = self::getFixture('unauthorized_api_key_result');
+            $r = (new Parser())->parse($data);
+            $this->fail('Should throw Auth exception');
+        } catch (AuthException $e) {
+            $this->assertEquals('Unauthorized (code: 40101)', $e->getMessage());
+        }
+    }
+
+    function testWithServiceUnavailableResult() {
+        try {
+            $data = self::getFixture('service_unavailable_result');
+            $r = (new Parser())->parse($data);
+            $this->fail('Should throw Server exception');
+        } catch (ServerException $e) {
+            $this->assertEquals('Authentication service unavailable (code: 50301)', $e->getMessage());
+        }
+    }
+
+    function testWithUndefinedErrorResult() {
+        try {
+            $data = self::getFixture('undefined_error_result');
+            $r = (new Parser())->parse($data);
+            $this->fail('Should throw Server exception');
+        } catch (ServerException $e) {
+            $this->assertEquals('Internal Server Error (code: 50001)', $e->getMessage());
+        }
+    }
+
+    function testWithNonXmlResult() {
+        try {
+            $data = self::getFixture('non_xml_result');
+            $r = (new Parser())->parse($data);
+            $this->fail('Should throw Server exception');
+        } catch (ServerException $e) {
+            $this->assertEquals('503 Service Unavailable', $e->getMessage());
+        }
+    }
+
+    function testWithLinks() {
         $data = self::getFixture('valid_links_result');
         $result = (new Parser())->parse($data);
         $actual_post = $result->posts[0];
@@ -139,10 +158,15 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         ], $actual_post->links);
     }
 
-    function testWithIncompleteResult()
-    {
+    function testWithIncompleteResult() {
         $data = self::getFixture('incomplete_result');
         $result = (new Parser())->parse($data);
         $this->assertTrue($result->incomplete());
+    }
+
+    function testWithCompleteResult() {
+        $data = self::getFixture('valid_empty_result');
+        $result = (new Parser())->parse($data);
+        $this->assertFalse($result->incomplete());
     }
 }
